c46b1d6202a2f6f320607c8b2210566040372f4b
Daniel Mills <mills.dma@gmail.com>
2013-10-15 10:53:03 -0400

    Update connectionManager.js

--- connectionManager.js
+++ connectionManager.js
@@ -1,9 +1,9 @@
 exports.c = function(){
 
 //setup mongo db
-	mongoose.connect('mongodb://nodejitsu:b33ab4e6b9397deddd0ef97ecb53faf9@alex.mongohq.com:10075/nodejitsudb1434696759',{
-		user:'nodejitsu',
-		pass:'b33ab4e6b9397deddd0ef97ecb53faf9'
+	mongoose.connect('mongodb://',{
+		user:'',
+		pass:''
 	});
 };
 
